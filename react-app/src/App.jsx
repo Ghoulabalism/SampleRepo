f9e00ec87b3d511c662267025b8082c4eb1d6244
Sabik <sabikfifa@gmail.com>
2026-04-02 13:26:41 -0500

    Updated h1 text content

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-    <h1 className='header'>Hello World!</h1>
+    <h1 className='header'>Hello from the React Application</h1>
     <div className='container'>
       <p className='para1'>Paragaraph 1 content</p>
       <p className='para2'>Paragraph 2 content</p>
